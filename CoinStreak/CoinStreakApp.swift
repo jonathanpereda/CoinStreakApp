274d725eaa9c3898aa1086d5251d50b1633d64c2
Jonathan Pereda <jpered4@wgu.edu>
2025-10-03 11:25:10 -0700

    Initial Commit

---
+++ CoinStreak/CoinStreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinStreakApp.swift
+//  CoinStreak
+//
+//  Created by Jonathan Pereda on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CoinStreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinStreakTests/CoinStreakTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoinStreakTests.swift
+//  CoinStreakTests
+//
+//  Created by Jonathan Pereda on 10/3/25.
+//
+
+import Testing
+@testable import CoinStreak
+
+struct CoinStreakTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
