0e95367be1050ae49841a95e0fff95e6f35c24f4
Jonathan Pereda <jpered4@wgu.edu>
2025-10-03 17:51:11 -0700

    Add main flip and streak logic

--- CoinStreak IOS/ContentView.swift
+++ CoinStreak IOS/ContentView.swift
@@ -8,15 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var streak = 0;
+    @State private var curState = "";
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text("Streak: \(streak)");
+            Text("COIN: \(curState)");
+            
+            Button("Flip") {
+                flipCoin()
+            }
         }
         .padding()
     }
+    
+    func flipCoin() {
+        let flip = Bool.random() ? "Heads" : "Tails"
+        if flip == curState {
+            streak += 1
+        } else {
+            streak = 1
+        }
+        curState = flip
+    }
+    
+    
+    
 }
 
 #Preview {
